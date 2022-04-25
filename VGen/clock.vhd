e8bfc9a5b44f8a700a66d52ff3a40559cc87b26c
ChrisDotVHDL <99539783+ChrisDotVHDL@users.noreply.github.com>
2022-04-25 17:21:04 +0200

    added io functionality

--- VGen/clock.vhd
+++ VGen/clock.vhd
@@ -2,6 +2,36 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity clock is
+  port(
+    input_0 : in std_logic;
+    input_1 : in std_logic;
+    input_2 : in std_logic;
+    input_3 : in std_logic;
+    input_4 : in std_logic;
+    input_5 : in std_logic;
+    input_6 : in std_logic;
+    input_7 : in std_logic;
+    input_8 : in std_logic;
+    input_9 : in std_logic;
+    input_10 : in std_logic;
+    input_11 : in std_logic;
+    output_0 : out std_logic;
+    output_1 : out std_logic;
+    output_2 : out std_logic;
+    output_3 : out std_logic;
+    output_4 : out std_logic;
+    output_5 : out std_logic;
+    output_6 : out std_logic;
+    output_7 : out std_logic;
+    output_8 : out std_logic;
+    output_9 : out std_logic;
+    output_10 : out std_logic;
+    output_11 : out std_logic;
+    output_12 : out std_logic;
+    output_13 : out std_logic;
+    output_14 : out std_logic;
+    output_15 : out std_logic
+  );
 end entity;
 
 architecture clock_arch of clock is
